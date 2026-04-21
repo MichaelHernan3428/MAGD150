953c0091294b4adcc6c1c177fadc08befa329750
MichaelHernan3428 <HernanMR23@uww.edu>
2026-04-21 14:58:17 -0500

    LabAssingment2
    Balloons in the sky

--- sketch.js
+++ sketch.js
@@ -1,57 +1,19 @@
-
-let snowballX;
-let snowballY;
-let windSpeed;
-let fallSpeed=2;
-let x = 200;
-let y = 200;
-let xSpeed = 2;
-let ySpeed = 3;
-
 function setup() {
-  createCanvas(400, 400);
-
-  fill(255);
-  snowballX = random(0,400);
-  snowballY = -5;
-  circle(snowballX,snowballY,20);
-  frameRate(200);
+   createCanvas(400, 400);
+   background(51);
+fill('red');
+curve(-150, 275,50,60,50,60,250,275);
+line(50,60,50,80);
+fill('green');
+  curve(-150,275,90,100,90,100,250,275);
+  line(90,100,90,120);
+fill('skyblue');
+curve(-150, 275, 110, 120, 110, 120, 350, 275);
+line(110, 120, 110, 140);
+fill('gray');
+triangle(200, 255, 215, 240, 220, 300);
+fill('yellow');
+quad(150, 150, 210, 150, 210, 210, 150, 210);
 }
-
 function draw() {
-  background(220);
-  circle(snowballX,snowballY,20);
-  windSpeed = 0;
-  //windSpeed = random(-1,2)
-  snowballX = snowballX + windSpeed;
-  snowballY = snowballY + fallSpeed;
-
-  if(snowballY >= height || snowballY<=-20 || snowballX<=0 || snowballX>= width){
-    //snowballX = random(0,400);
-    //snowballY = -5;
-    fallSpeed = -fallSpeed; }
- { fill(255);
-  stroke(15);
-  circle(x, y, 50);
-  frameRate(50);
-  
-  x += xSpeed;
-  y += ySpeed;
-  
-  if (x > width || x < 0) xSpeed *= -1;
-  if (y > height || y < 0) ySpeed *= -1; }
-/*
-if(snowballY == height){
-    snowballX = random(0,400);
-    snowballY = -5;
-  }
-  if(snowballX >= width){
-    snowballX = random(0,400);
-    snowballY = -5;
-  }
-  if(snowballX <=0){
-    snowballX = random(0,400);
-    snowballY = -5;  
-  }
-*/
 }
