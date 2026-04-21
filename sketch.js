b22bf62627e0f709d13b8bfb943ace774481b10f
MichaelHernan3428 <HernanMR23@uww.edu>
2026-04-21 14:55:41 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,40 +1,57 @@
+
+let snowballX;
+let snowballY;
+let windSpeed;
+let fallSpeed=2;
+let x = 200;
+let y = 200;
+let xSpeed = 2;
+let ySpeed = 3;
+
 function setup() {
   createCanvas(400, 400);
+
+  fill(255);
+  snowballX = random(0,400);
+  snowballY = -5;
+  circle(snowballX,snowballY,20);
+  frameRate(200);
 }
 
 function draw() {
-  background(51);
-  colorMode(255, 255, 255, 1);
-  let x = frameCount % 100;
-  frameRate(10);
-  frameRate(60);
-  circle(x, 50, 20);
-  fill(0)
-  text(`${mouseX}, ${mouseY}`, 20, 20);
-    fill(255);
-  stroke(0);
-  circle(350, 50, 100);
-   stroke("navy");   
-  fill("navy");
-  circle(320,50,100);
-  frameRate(10);
+  background(220);
+  circle(snowballX,snowballY,20);
+  windSpeed = 0;
+  //windSpeed = random(-1,2)
+  snowballX = snowballX + windSpeed;
+  snowballY = snowballY + fallSpeed;
 
-  describe('A line follows the mouse as it moves. The line grows longer with faster movements.');
-line(pmouseX, pmouseY, mouseX, mouseY);
-    let base = 3;
-  let d = pow(base, 255);
-  circle(10, 10, d);
-  d = pow(base, 2);
-  circle(20, 20, d);
-  d = pow(base, 3);
-  circle(40, 40, d);
-  d = pow(base, 4);
-  circle(80, 80, d);
-
-  describe('A series of circles that grow exponentially from top left to bottom right.');
-  let c = sqrt(16);
-  circle(33, 33, d);
-  d = sqrt(1600);
-  circle(67, 67, d);
- describe('Two white circles. The circle at the bottom-left is small. The circle at the bottom-right is ten times larger.');
+  if(snowballY >= height || snowballY<=-20 || snowballX<=0 || snowballX>= width){
+    //snowballX = random(0,400);
+    //snowballY = -5;
+    fallSpeed = -fallSpeed; }
+ { fill(255);
+  stroke(15);
+  circle(x, y, 50);
+  frameRate(50);
+  
+  x += xSpeed;
+  y += ySpeed;
+  
+  if (x > width || x < 0) xSpeed *= -1;
+  if (y > height || y < 0) ySpeed *= -1; }
+/*
+if(snowballY == height){
+    snowballX = random(0,400);
+    snowballY = -5;
+  }
+  if(snowballX >= width){
+    snowballX = random(0,400);
+    snowballY = -5;
+  }
+  if(snowballX <=0){
+    snowballX = random(0,400);
+    snowballY = -5;  
+  }
+*/
 }
