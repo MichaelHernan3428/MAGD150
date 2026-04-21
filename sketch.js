c01f78d06fcf17b3668ae64f0e554ef3955bada2
MichaelHernan3428 <HernanMR23@uww.edu>
2026-04-21 15:03:28 -0500

    LabAssingment2
    Balloons in the sky

--- sketch.js
+++ sketch.js
@@ -1,40 +1,19 @@
 function setup() {
-  createCanvas(400, 400);
+   createCanvas(400, 400);
+   background(51);
+fill('red');
+curve(-150, 275,50,60,50,60,250,275);
+line(50,60,50,80);
+fill('green');
+  curve(-150,275,90,100,90,100,250,275);
+  line(90,100,90,120);
+fill('skyblue');
+curve(-150, 275, 110, 120, 110, 120, 350, 275);
+line(110, 120, 110, 140);
+fill('gray');
+triangle(200, 255, 215, 240, 220, 300);
+fill('yellow');
+quad(150, 150, 210, 150, 210, 210, 150, 210);
 }
-
 function draw() {
-  background(51);
-  colorMode(255, 255, 255, 1);
-  let x = frameCount % 100;
-  frameRate(10);
-  frameRate(60);
-  circle(x, 50, 20);
-  fill(0)
-  text(`${mouseX}, ${mouseY}`, 20, 20);
-    fill(255);
-  stroke(0);
-  circle(350, 50, 100);
-   stroke("navy");   
-  fill("navy");
-  circle(320,50,100);
-  frameRate(10);
-
-  describe('A line follows the mouse as it moves. The line grows longer with faster movements.');
-line(pmouseX, pmouseY, mouseX, mouseY);
-    let base = 3;
-  let d = pow(base, 255);
-  circle(10, 10, d);
-  d = pow(base, 2);
-  circle(20, 20, d);
-  d = pow(base, 3);
-  circle(40, 40, d);
-  d = pow(base, 4);
-  circle(80, 80, d);
-
-  describe('A series of circles that grow exponentially from top left to bottom right.');
-  let c = sqrt(16);
-  circle(33, 33, d);
-  d = sqrt(1600);
-  circle(67, 67, d);
- describe('Two white circles. The circle at the bottom-left is small. The circle at the bottom-right is ten times larger.');
 }
